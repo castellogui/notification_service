c0ea440f74396f4eaba5412ae2f0bc4cae98e370
Guilherme Castello <guilhermecastellobrasolin@gmail.com>
2026-02-09 17:26:02 -0300

    feat: update Writer interface for enhanced notification management

--- internal/pusher/interfaces/notification_db.go
+++ internal/pusher/interfaces/notification_db.go
@@ -6,8 +6,11 @@ import (
 )
 
 type Writer interface {
-	SaveNotification(ctx context.Context, n domain.NotificationDB) error
+	SaveNotification(ctx context.Context, n domain.NotificationDB) (domain.NotificationDB, error)
 	GetNotification(ctx context.Context, id string, userID string) (domain.NotificationDB, error)
+	UpdateNotification(ctx context.Context, userID string, id string, fields map[string]interface{}) (domain.NotificationDB, error)
 	DeleteNotification(ctx context.Context, id string) error
 }
+
+// TODO: Remove this from pusher cause is used also in API
 
