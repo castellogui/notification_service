ca0951b037830daffd958ed049fd993ef54712d2
Guilherme Castello <guilhermecastellobrasolin@gmail.com>
2026-02-09 08:56:36 -0300

    feat: create envelope.go and viewmodel.go

---
+++ internal/notification/domain/envelope.go
@@ -0,0 +1,19 @@
+package domain
+
+import (
+	"encoding/json"
+	"time"
+)
+
+type Kind string
+type Channel string
+
+type Envelope struct {
+	ID				string			`json:"id"`
+	UserID			string  		`json:"userid"`
+	Kind			Kind			`json:"kind"`
+	Version 		int				`json:"version"`
+	DeliverAt		*time.Time		`json:"deliverAt,omitempty"`
+	ChannelHints 	[]Channel		`json:"channelHints,omitempty"`
+	Payload			json.RawMessage	`json:"payload"`
+}
---
+++ internal/notification/domain/viewmodel.go
@@ -0,0 +1,9 @@
+package domain
+
+type ViewModel struct {
+	Title    string
+	Body     string
+	Category string
+	DeepLink string
+	Data     map[string]string
+}
